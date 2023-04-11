e678108ced242c54278b0141138acf5f0d37a299
pavan <pavankumarpatnala96@gmail.com>
2023-04-11 06:07:00 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div>
+    <h1>Congratulations</h1>
+    <div className="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Kiran</h2>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
